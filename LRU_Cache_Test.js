6e4b94f8c4284adf4eedcf3b40200eb851ec88f6
kinua-byte <64353156+kinua-byte@users.noreply.github.com>
2021-04-30 22:17:58 +0900

    4/30/a

--- LRU_Cache_Test.js
+++ LRU_Cache_Test.js
@@ -1,15 +1,12 @@
 class Box {
-  public void show() {
-    System.out.println()
-  }
 
 }
 //入れ物を作る
 //test
-function accepta(a) {
-  Box box = new Box();
- Box.push(a)  
+function accepta() {
+  const box = [ ];
+  box.push('a')  ;
 
-  assertEquals("a",box.show())
+  assertEquals('a',box.show())
 }
 module.experts = accepta
--- LRU_Cache_Test.test.js
+++ LRU_Cache_Test.test.js
@@ -2,5 +2,5 @@ const { test, expect } = require("@jest/globals");
 const { accepta } = require("./LRU_Cache_Test");
 
 test('a equal a', () => {
-  expect(accepta(1)).toBe(1)
+  expect(box).toContain(a);
 } )
