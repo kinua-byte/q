bfea411d8f96c65b880d59a58bcadc6939e31411
kinua-byte <64353156+kinua-byte@users.noreply.github.com>
2021-04-30 13:24:01 +0900

    javaのように書いた

--- LRU_Cache_Test.js
+++ LRU_Cache_Test.js
@@ -1,6 +1,15 @@
+class Box {
+  public void show() {
+    System.out.println()
+  }
+
+}
 //入れ物を作る
 //test
 function accepta(a) {
-  return a
+  Box box = new Box();
+ Box.push(a)  
+
+  assertEquals("a",box.show())
 }
 module.experts = accepta
