7d504692794bb95513685edb7c401a8666cb420f
kinua-byte <64353156+kinua-byte@users.noreply.github.com>
2021-05-13 19:59:09 +0900

    5/13/a

--- LRU_Cache.test.js
+++ LRU_Cache.test.js
@@ -1,22 +1,3 @@
-// const { test, expect } = require("@jest/globals");
-// var box = require('./LRU_Cache_Test.js');
-
-// test('boxを作る ', () => {
-
-//   // const box = [ ];
-
-//   // box.push('a')  ;
-//   //検証(assert)
-//   //aが入っているか確認
-//   //expect(box.obj.class.array).toContain('a');
-//   //要素数確認
-//   expect(box.obj.class.array).toHaveLength(0);
-// } )
-// test('aを追加する',() => {
-//     expect(box.obj.class.array.push).toHaveLength(1);
-// })
-
-//new
 const LRU = require("./LRU_Cache");
 
 test("登録した要素を取得できる", () => {
